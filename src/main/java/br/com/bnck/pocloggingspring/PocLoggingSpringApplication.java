7e03950caf625fc5dfae5ea45d274d453a4f6baf
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-07-03 12:00:20 -0300

    Initial commit

---
+++ src/main/java/br/com/bnck/pocloggingspring/PocLoggingSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.bnck.pocloggingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLoggingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLoggingSpringApplication.class, args);
+	}
+
+}
